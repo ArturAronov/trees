5b6d6031ace289f12be3a638006cea69a1afcec5
Artur Aronov <unionco@icloud.com>
2025-10-26 01:11:29 +0800

    main: add inorder test

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"trees-go/dfs/in_order"
+	"trees-go/utils"
+)
+
+func main() {
+	node := utils.Add([]int{6, 4, 7, 3, 2, 8, 2, 35, 9, 3, 11, 75, 32, 1, 33})
+	in_order.InOrderSearch(*node)
+}
